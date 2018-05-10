6736143ecce5cfb5f24e6633c7daa01c1b7f5ae8
Meyta <meytajennist@gmail.com>
2018-05-10 15:39:57 +0700

    add dummy content

--- app/src/main/java/id/mzennis/m_health/MainActivity.java
+++ app/src/main/java/id/mzennis/m_health/MainActivity.java
@@ -1,11 +1,29 @@
 package id.mzennis.m_health;
 
+import android.app.AlertDialog;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.media.MediaPlayer;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.NotificationCompat;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends BaseActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,5 +39,54 @@ public class MainActivity extends BaseActivity {
             }
         });
 
+//        sendNotification("Yeay! it's time to breakfast, \nbefore you eat do not forget to enter glucose level! ^^");
+        sendNotification("Your glucose level is high :(");
+    }
+
+    private void buildAlert(String msg) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Alarm")
+                .setMessage(msg)
+        .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        })
+        .show();
+    }
+
+    long[] v = {500,1000};
+
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
+    private void sendNotification(String msg) {
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(getApplicationContext(), "notify_001");
+        Intent ii = new Intent(getApplicationContext(), MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, ii, 0);
+
+        Uri uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
+
+        mBuilder.setContentIntent(pendingIntent);
+        mBuilder.setSmallIcon(R.mipmap.ic_launcher_round);
+        mBuilder.setContentTitle("Alarm");
+        mBuilder.setContentText(msg);
+        mBuilder.setVibrate(v);
+//        mBuilder.setSound(uri);
+        mBuilder.setPriority(Notification.PRIORITY_MAX);
+
+        NotificationManager mNotificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel("notify_001",
+                    "Channel human readable title",
+                    NotificationManager.IMPORTANCE_DEFAULT);
+            mNotificationManager.createNotificationChannel(channel);
+        }
+
+        mNotificationManager.notify(0, mBuilder.build());
+
+        buildAlert(msg);
     }
 }
